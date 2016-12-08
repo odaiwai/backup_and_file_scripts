8abc4474dfcdbdf08804d39f70cb335d3263ce9e
Dave O'Brien <odaiwai@diaspoir.net>
2016-12-08 11:19:21 +0800

    reduced the output of the btrfs backup, and changed the backup location to /backup/BACKUP

--- make_backup_btrfs.pl
+++ make_backup_btrfs.pl
@@ -1,24 +1,32 @@
 #!/usr/bin/perl
+use strict;
+use warnings;
 
 # 20150710: This should really check if the backup drive is available
 # 20151121: changed to perl
 # 20160102: this has stopped working, probably as there's some disconnect
 # 	    with the parent ID of the subvolumes
 # 20160828: try to enhance the subvolume backups to work properly
+# 20160929: check for the existence of the /backups volume
 #
 my $for_real = 1;
-my $verbose = 1;
+my $verbose = 0;
 my $first_run = 0;
 my $local = "home";
 my $remote = "backup";
 
+my $backup_vol_present = `df | grep backup | wc -l`;
+if ($backup_vol_present == 0) {
+	print "Backup Volume not present. Aborting.\n" if $verbose;
+	exit;
+}
 my $logfile = "/root/backups/backup.log";
 open ( my $log, ">", $logfile) or die "Can't open $logfile\n";
 
 if ($first_run) {
 	# Nuke the backups and start again
 	printlog($log, "First Run: deleting the backups!");
-	my $result = do_cmd("btrfs subvolume delete /$remote/$local/BACKUP");
+	my $result = do_cmd("btrfs subvolume delete /$remote/BACKUP");
 	$result = do_cmd("btrfs subvolume delete /$local/BACKUP");
 }
 
@@ -37,32 +45,32 @@ if ( !(-d "/$local/BACKUP")) {
 }
 
 # Stage 2 if /$local/BACKUP exists and /$remote/$local/BACKUP doesn't, send|receive it
-printlog ($log, "2. Check if /$local/BACKUP exists and /$remote/$local/BACKUP doesn't.");
-if ( (-d "/$local/BACKUP") && !(-d "/$remote/$local/BACKUP") ) {
+printlog ($log, "2. Check if /$local/BACKUP exists and /$remote/BACKUP doesn't.");
+if ( (-d "/$local/BACKUP") && !(-d "/$remote/BACKUP") ) {
 	#Copy to $remote 
 	printlog( $log, "\t2.1 Backup only exists locally: sending...");
-	my $result = do_cmd("btrfs send /$local/BACKUP | btrfs receive /$remote/$local");
+	my $result = do_cmd("btrfs send /$local/BACKUP | btrfs receive /$remote");
 } 
 # Stage 3: if both $local/BACKUP and $remote$local/BACKUP exist do an incremental backup
 # This is what will happen after every initial backup
-printlog ($log, "3. Check if /$local/BACKUP and /$remote/$local/BACKUP both exist.");
-if ( -d ("/$local/BACKUP") && -d "/$remote/$local/BACKUP") {
+printlog ($log, "3. Check if /$local/BACKUP and /$remote/BACKUP both exist.");
+if ( -d ("/$local/BACKUP") && -d "/$remote/BACKUP") {
 	# incremental backup
 	printlog ($log, "\t3.1 Local and Remote Backups Exist: making incremental btrfs backup of /$local...");
 	my $result = do_cmd("btrfs subvolume snapshot -r /$local /$local/BACKUP-new");
 	$result = do_cmd("sync");
-	$result = do_cmd("btrfs send -p /$local/BACKUP /$local/BACKUP-new | btrfs receive /$remote/$local");
+	$result = do_cmd("btrfs send -p /$local/BACKUP /$local/BACKUP-new | btrfs receive /$remote");
 	if ( (-d "/$local/BACKUP") and (-d "/$local/BACKUP-new")) { 
 		# clean up and increment our backup
 		printlog ($log, "3.1.1 Local Backup has old and new versions: cleaning up /$local");
 		$result = do_cmd("btrfs subvolume delete /$local/BACKUP");
 		$result = do_cmd("mv /$local/BACKUP-new /$local/BACKUP");
 	}
-	if ( (-d "/$remote/$local/BACKUP") and (-d "/$remote/$local/BACKUP-new")) { 
+	if ( (-d "/$remote/BACKUP") and (-d "/$remote/BACKUP-new")) { 
 		# and clean it up from the backup as well
 		printlog ($log, "3.1.2 Remote has old and new backups: cleaning up /$remote...");
-		$result = do_cmd("btrfs subvolume delete /$remote/$local/BACKUP");
-		$result = do_cmd("mv /$remote/$local/BACKUP-new /$remote/$local/BACKUP");
+		$result = do_cmd("btrfs subvolume delete /$remote/BACKUP");
+		$result = do_cmd("mv /$remote/BACKUP-new /$remote/BACKUP");
 	}
 }
 
@@ -85,7 +93,7 @@ sub printlog {
 	my $logfh = shift;
 	my $statement = shift;
 	print $logfh "$statement\n";
-	print "$statement\n";
+	print "$statement\n" if $verbose;
 	return 1;
 }
 
