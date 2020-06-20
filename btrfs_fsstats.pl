5b6ecca966bd97b201ba5b73a3bdd84f98f51169
Dave O'Brien <github@diaspoir.net>
2020-06-20 17:43:27 +0800

    Tidied up the output a bit, and added the pretty_bytes function.

--- btrfs_fsstats.pl
+++ btrfs_fsstats.pl
@@ -19,7 +19,7 @@ my %pool_type_bal;
 
 foreach my $pool (@pools) {
     chomp $pool;
-    print "Pool: $pool\n";
+	#print "Pool: $pool\n";
     my @results = `df -B1 $pool; btrfs fi df -b $pool`;
     foreach my $line (@results) {
         chomp $line;
@@ -39,7 +39,7 @@ foreach my $pool (@pools) {
 				#print "$type, $format, $total, $used, " . sprintf( "%0.2f", $pct ) . "%\n";
 				$pool_type_size{"$pool\_$type"} = $total;
 				$pool_type_used{"$pool\_$type"} = $used;
-				$pool_type_bal{"$pool\_$type"} = sprintf( "%0.2f", $pct) . "%";
+				$pool_type_bal{"$pool\_$type"} = sprintf( "%0.1f", $pct) . "%";
         }
     }
 	my $total_used = $pool_type_used{$pool."_Data"} + $pool_type_used{$pool."_Metadata"} + $pool_type_used{$pool."_System"};
@@ -47,6 +47,32 @@ foreach my $pool (@pools) {
 	my $pool_Data_pct = sprintf ("%0.1f", ( 100 * $pool_type_size{$pool."_Data"} / $pool_size{$pool} )) . "%";
 	my $pool_Meta_pct = sprintf ("%0.1f", ( 100 * $pool_type_size{$pool."_Metadata"} / $pool_size{$pool} )) . "%";
 	my $pool_Syst_pct = sprintf ("%0.1f", ( 100 * $pool_type_size{$pool."_System"} / $pool_size{$pool} )) . "%";
-	print "$pool ($pool_mounts{$pool}) $total_used Data/Meta/Sys: ($pool_Data_pct/$pool_Meta_pct/$pool_Syst_pct) Balanced:($pool_type_bal)\n"; 
+	my $pretty_total = pretty_bytes($total_used);
+	print "$pool\t($pool_mounts{$pool}) $pretty_total Data/Meta/Sys: ($pool_Data_pct/$pool_Meta_pct/$pool_Syst_pct) Balanced:($pool_type_bal)\n"; 
+}
+
+sub pretty_bytes {
+	# given a number of b, return a nice looking number like 1.21 GB
+	# optional: use powers of ten instead?
+	my $size = shift;
+	my $scale = shift;
+	my $base = 1024;
+	my @suffixes=("B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"); # Bytes
+	my @longsuffixes=("Bytes", "Kibibytes", "Mebibytes", "Gibibytes", "Tebibytes", "Pebibytes", "Exbibytes", "Zebibytes", "Yobibytes"); # Bytes
+	if (defined($scale) and $scale == 1) {
+		# using powers of 10
+		$base = 1000;
+		@suffixes=("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"); # Bytes
+		@longsuffixes=("Bytes", "Kilobytes", "Megabytes", "Gigabytes", "Terabytes", "Petabytes", "Exabytes", "Zettabytes", "Yottabytes"); # Bytes
+	}
+	$size *= $base ; # Usage reported in KB as default
+	my $new_size;
+	$size /= $base; # divide to get KiB/KB
+	for ( my $power=0; $power<9; $power++) {
+		my $divisor = ($base) ** $power;
+		#print "1024 ^ $power = $divisor\n";
+		if ( $size > $divisor ) { $new_size=sprintf ( "%.2f",($size / $divisor)) . "$suffixes[$power]" ; }
+	}
+	return $new_size;
 }
 
