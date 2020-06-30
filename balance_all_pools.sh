8353ed32b98f4bcdfccb40299ec88dc8c0d35d0a
Dave O'Brien <odaiwai@diaspoir.net>
2020-06-30 15:57:24 +0800

    Added some options to the balancing script.

--- balance_all_pools.sh
+++ balance_all_pools.sh
@@ -1,30 +1,69 @@
 #!/bin/bash
 
-POOLS=`mount| grep btrfs | cut -d' ' -f3`
+
+POOLS=`mount| grep btrfs | cut -d' ' -f3 | tr '\n' ' '`
+
+# defaults
 LIMIT=1
-PASSES=100
-PAUSE=1
+PASSES=1
+PAUSE=10
+FOR_REAL=1
 I_AM=`whoami`
 
+# Get the arguments in pairs
+
+while [[ $# -gt 0 ]]
+do
+	key="$1"
+	case $key in
+		-l|--limit)
+			LIMIT="$2"
+			shift
+			shift
+			;;
+		-p|--passes)
+			PASSES="$2"
+			shift
+			shift
+			;;
+		-s|--sleep|--pause)
+			PAUSE="$2"
+			shift
+			shift
+			;;
+		-d|--dryrun|--dry-run)
+			FOR_REAL=0
+			shift
+			;;
+	*)
+			shift
+			;;
+	esac
+done
+
+SUDO="sudo"
 if [[ $I_AM = "root" ]]
 then
 		SUDO=""
-else
-		SUDO="sudo "
 fi
-echo "Performing $PASSES balances on $POOLS (limit $LIMIT) with $PAUSE second pauses. Running as $I_AM."
+
+# Do the deed
+echo "Performing $PASSES balances on [$POOLS] (limit $LIMIT) with $PAUSE second pauses as user '$I_AM'. Dry Run Status: $FOR_REAL"
 date
 
 for PASS in `seq 1 $PASSES`
 do
 	for POOL in $POOLS
 	do
-			echo "Balancing $POOL, Pass $PASS"
-			CMD="$SUDO btrfs balance start -v $POOL -dlimit=$LIMIT -mlimit=$LIMIT"
-			date
-			echo "$CMD"
-			`time $CMD`
-			echo "Status: $?, sleeping for $PAUSE seconds..."
-			sleep $PAUSE
+		echo "Balancing $POOL, Pass $PASS/$PASSES"
+		CMD="$SUDO btrfs balance start -v $POOL -dlimit=$LIMIT -mlimit=$LIMIT"
+		date
+		echo "$CMD"
+		if [[ $FOR_REAL ]]
+		then
+			time $CMD
+		fi
+		echo "Status: $?, sleeping for $PAUSE seconds..."
+		sleep $PAUSE
 	done
 done
