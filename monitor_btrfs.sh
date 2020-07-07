4e8e845145d1806f19e118453bf444888dfd5b2f
Dave O'Brien <github@diaspoir.net>
2020-07-07 13:03:35 +0800

    Updated the Monitor Script.

--- monitor_btrfs.sh
+++ monitor_btrfs.sh
@@ -20,8 +20,18 @@ do
 	/home/odaiwai/src/backup_and_file_scripts/btrfs_fsstats.pl
 	for POOL in $POOLS
 	do
-		echo "# btrfs scrub status $POOL:"
-		btrfs scrub status $POOL
+		CMD="btrfs scrub status $POOL"
+		echo -n "# $CMD: "
+		RESULT=`$CMD`
+		SCRUB_STATUS=`$CMD | grep unning | wc -l`
+		SCRUB_RESULT=`$CMD | grep summary`
+		if [[ $SCRUB_STATUS -gt 0 ]]
+		then
+			echo "Running..."
+			echo "$RESULT"
+		else
+			echo "Finished: $SCRUB_RESULT"
+		fi
 	done
 
 	echo -n "sleeping for $PAUSE seconds..."
