99d3cc75c621cd3c7a991b6f368dd609e4e9631a
Dave O'Brien <odaiwai@diaspoir.net>
2021-04-20 14:45:59 +0800

    find/restore CSUM error utilities.

--- find_damaged_files.sh
+++ find_damaged_files.sh
@@ -2,23 +2,62 @@
 
 use strict;
 use warnings;
+my $for_real = 1;
+#
+# check the dmesg log for BTRFS errors and delete the file
+# should do a:
+#	btrfs filesystem sync /home 
+# after
 
 # better regexp:
 #           `dmesg | grep path: | sed -E 's/^.*\(path: (.*)\)$/\1/;' | sort |uniq > damaged_files.txt`;
-my @files = `cat damaged_files.txt`;
-# provides a list that will need to be escaped for shell purposes (contains spaces)
+my %top_levels;
+my %files;
+print "Reading dmesg...";
+my @lines = `dmesg -T | grep BTRFS`;
+for my $line (@lines) {
+	chomp $line;
+	if ( $line =~ /\(path: (.*)\)/ )  {
+		my $file = "/home/$1";
+		$files{$file}++;
+	}
+if  ( $line =~ /corrupt ([0-9]+)/ ) {
+		my $inode = $1;
+		my $file = `sudo btrfs inspect-internal inode-resolve $inode /home`;
+		#print "INODE: $inode -> $file\n";
+		chomp $file;
+		$files{$file}++;
+	}
+}
+print "\n";
+
+my @files = keys(%files); #`cat damaged_files.txt`;
 # algorithm:
 for my $file (@files) {
 	chomp $file;
-	my $filepath = "/home/$file";
-	print "FILE: $filepath ... ";
-	if ( -e $filepath ) {
+	print "FILE: $file ($files{$file})... ";
+	my @parts = split "/", $file;
+	my $top_level = "/$parts[1]/$parts[2]/$parts[3]/$parts[4]";
+	#print "@parts, $top_level\n";
+	$top_levels{$top_level}++;
+	if ( -e $file ) {
 		print "exists.\n";
-		my $cmd = "rm -v \"$filepath\"";
+		my $cmd = "sudo rm -rfv \"$file\"";
 		print "$cmd";
-		my $results = `time $cmd`;
-		print "\t$results\n";
+		if ($for_real) {
+			my $results = `time $cmd`;
+			print "\t$results\n";
+		}
 	} else {
 		print "not there!\n";
 	}	
+}
+print "Syncing /home ... ";
+my $results = `sudo btrfs filesystem sync /home`;
+print "$results\n";
+
+# print out the top levels to be restored.
+my @top_levels = keys(%top_levels);
+for my $top_level (@top_levels) {
+		print "$top_level ($top_levels{$top_level})\n";
 }
--- restore_home.pl
+++ restore_home.pl
@@ -11,14 +11,15 @@ my $rsync_options = "--stats --compress --recursive --times --perms --links --hu
 $rsync_options .= " --progress --info=progress2" if $verbose;
 $rsync_options .= " --dry-run" if $dry_run;
 
-my $src_dir = "/backup/BACKUP.20210415_0338/odaiwai";
-my $home_dir = "/home/odaiwai";
+my $src_dir = "/backup/BACKUP.20210415_0338";
+my $home_dir = "/home";
 my @dirs = return_dirs();
 
 for my $dir (@dirs) {
 	chomp $dir;
-	print "copying $src_dir/$dir to $home_dir ...\n";
-	my $cmd = "rsync $rsync_options $src_dir/$dir $home_dir";
+
+	print "copying $src_dir/$dir/ to $home_dir/$dir ...\n";
+	my $cmd = "rsync $rsync_options $src_dir/$dir/ $home_dir/$dir";
 	print "$cmd\n";
 	my $result = `time $cmd`;
 	print "$result\n";
@@ -36,7 +37,17 @@ sub return_dirs {
 		@dirs = `ls -atr $src_dir`;
 	} else {
 		#@dirs = qw/Documents Games Pictures RaspberryPi webcam iPhone_utilities projects/; # list of dirs to work with
-		@dirs = qw/Downloads Music Pictures/; # list of dirs to work with
+		push @dirs, "conor/windows_backup";
+		push @dirs, "odaiwai/Downloads/Wii";
+		push @dirs, "odaiwai/Music/iTunes";
+		push @dirs, "odaiwai/Pictures/Ricoh_GX200";
+		push @dirs, "odaiwai/Music/aiff_music";
+		push @dirs, "odaiwai/Documents/Transport_Planning";
+		push @dirs, "odaiwai/Pictures/iPhoto Library";
+		push @dirs, "odaiwai/Music/SXSW_2012_Showcasing_Artists_Part2";
+		push @dirs, "odaiwai/Pictures/Photos Library.photoslibrary";
+		push @dirs, "odaiwai/Music/SXSW_2006_Showcasing_Artists_-_Release_1";
+		push @dirs, "odaiwai/Music/iTunes";
 	}
 	return @dirs;
 }
