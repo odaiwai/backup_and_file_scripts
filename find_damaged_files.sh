8edbcc86ed949bdca61c64acf8073ecbe8138816
Dave O'Brien <github@diaspoir.net>
2021-07-27 09:46:46 +0800

    updated find damage utility

--- find_damaged_files.sh
+++ find_damaged_files.sh
@@ -4,19 +4,24 @@ use strict;
 use warnings;
 my $for_real = 1;
 #
+# (c) dave o'brien 20210420
+#
 # check the dmesg log for BTRFS errors after a scrub and delete the file
 # should do a:
 #	btrfs filesystem sync /home 
-# after
+# after deleting files before scrubbing again.
 
 # other way to root out csum issues:
 # https://www.reddit.com/r/btrfs/comments/hcllay/how_to_cleanup_after_btrfs_scrub_checksum_errors/
-# cd ~/; find . -execdir cat {} + > /dev/null
-# this tries to cat each file in the filesystem to /dev/null
+#
+# cd ~/; find -type f .exec {} + -exec md5sum {} + 
+#
+# # this tries to read each file in the filesystem
 # it will trigger a csum error in dmesg if there's a problem
-# 
-# better regexp:
-#           `dmesg | grep path: | sed -E 's/^.*\(path: (.*)\)$/\1/;' | sort |uniq > damaged_files.txt`;
+# It will take a long time!
+#
+# When scrub runs with no errors, the restore_home.pl utility can restore the deleted files from the backup.  Very manual process so adjust it to suit.
+#
 my %top_levels;
 my %files;
 print "Reading dmesg...";
@@ -27,7 +32,25 @@ for my $line (@lines) {
 		my $file = "/home/$1";
 		$files{$file}++;
 	}
-if  ( $line =~ /corrupt ([0-9]+)/ ) {
+	if  ( $line =~ /ino ([0-9]+) off/ ) {
+		my $inode = $1;
+		my $result = `sudo btrfs inspect-internal inode-resolve $inode /home 2>&1`;
+		if ( $? == 0 ) {
+			chomp $result;
+			print "INODE: $inode -> $result\n";
+			$files{$result}++;
+		}
+	}
+#	if  ( $line =~ /logical ([0-9]+) on/ ) {
+#		my $logical = $1;
+#		my $result = `sudo btrfs inspect-internal logical-resolve $logical /home 2>&1`;
+#		if ( $? == 0 ) {
+#			chomp $result;
+#			print "LOGICAL: $logical -> $result\n";
+#			$files{$result}++;
+#		}
+#	}
+if  ( $line =~ /inode ([0-9]+), offset/ ) {
 		my $inode = $1;
 		my $result = `sudo btrfs inspect-internal inode-resolve $inode /home 2>&1`;
 		if ( $? == 0 ) {
@@ -40,27 +63,28 @@ if  ( $line =~ /corrupt ([0-9]+)/ ) {
 print "\n";
 
 my @files = keys(%files); #`cat damaged_files.txt`;
+my $deleted = 0;
 # algorithm:
 for my $file (@files) {
 	chomp $file;
-	print "FILE: $file ($files{$file})... ";
 	my @parts = split "/", $file;
 	my $top_level = "/$parts[1]/$parts[2]/$parts[3]/$parts[4]";
 	#print "@parts, $top_level\n";
 	$top_levels{$top_level}++;
 	if ( -e $file ) {
-		print "exists.\n";
+		print "FILE: $file ($files{$file})... exists.\n";
 		my $cmd = "sudo rm -rfv \"$file\"";
 		print "$cmd";
 		if ($for_real) {
 			my $results = `time $cmd`;
 			print "\t$results\n";
+			$deleted++;
 		}
 	} else {
-		print "not there!\n";
+		print "$file not there!\n";
 	}	
 }
-print "Syncing /home ... ";
+print "Deleted $deleted files. Syncing /home ... ";
 my $results = `sudo btrfs filesystem sync /home`;
 print "$results\n";
 
