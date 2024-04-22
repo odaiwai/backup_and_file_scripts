cf0ef00b0add243b1b5880a9425fd4255cce4587
Dave O'Brien <github@diaspoir.net>
2024-04-22 15:54:40 +0800

    Cleaned up the monitor shell script.

--- monitor_btrfs.sh
+++ monitor_btrfs.sh
@@ -1,55 +1,47 @@
 #!/bin/bash
 
-
 PASSES=100
 PAUSE=300
 INTERVAL=5
-I_AM=`whoami`
-if [[ $I_AM = "root" ]]
-then
-		SUDO=""
+if [[ "$(whoami)" = "root" ]]; then
+	SUDO=""
 else
-		SUDO="sudo "
+	SUDO="sudo "
 fi
 
-while [[ true ]]
-do
+while [[ true ]]; do
 	clear
 	date
 	# Rescan the pools inside the loop
-	POOLS=`mount| grep btrfs | cut -d' ' -f3`
-	df -h / $POOLS
+	POOLS=$(mount | grep btrfs | cut -d' ' -f3)
+	df -h / "$POOLS"
 	/home/odaiwai/src/backup_and_file_scripts/btrfs_fsstats.pl
-	for POOL in $POOLS
-	do
+	for POOL in $POOLS; do
 		CMD="btrfs scrub status $POOL"
 		echo -n "# $CMD: "
-        RESULT=`$CMD`
-        SCRUB_STATUS=`$CMD | grep unning | wc -l`
-        SCRUB_RESULT=`$CMD | grep summary`
-        if [[ $SCRUB_STATUS -gt 0 ]]
-         then
-                echo "Running..."
-                echo "$RESULT"
-        else
-                echo "Finished: $SCRUB_RESULT"
-        fi
-		
+		RESULT=$($CMD)
+		SCRUB_STATUS=$($CMD | grep -c unning)
+		SCRUB_RESULT=$($CMD | grep summary)
+		if [[ $SCRUB_STATUS -gt 0 ]]; then
+			echo "Running..."
+			echo "$RESULT"
+		else
+			echo "Finished: $SCRUB_RESULT"
+		fi
+
 		CMD="btrfs balance status $POOL"
-		RESULT=`$CMD`
-		BAL_STATUS=`$CMD | grep unning | wc -l`
-		if [[ $BAL_STATUS -gt 0 ]]
-		then
+		RESULT=$($CMD)
+		BAL_STATUS=$($CMD | grep -c unning)
+		if [[ $BAL_STATUS -gt 0 ]]; then
 			echo "$RESULT"
 		fi
-done
+	done
 
-sensors | grep -E  '(Core|[0-9]{2,} RPM)'
-# btrfs fi show /home
-    for NOW in `seq $PAUSE -$INTERVAL 0`
-    do
-	    echo -ne "sleeping for $NOW seconds...\r"
-    	sleep $INTERVAL
-    done
-    echo
+	sensors | grep -E '(Core|[0-9]{2,} RPM)'
+	# btrfs fi show /home
+	for NOW in $(seq $PAUSE -$INTERVAL 0); do
+		echo -ne "sleeping for $NOW seconds...\r"
+		sleep $INTERVAL
+	done
+	echo
 done
