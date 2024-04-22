15655b413cc25430525bcbab1a25760b8e9f1811
Dave O'Brien <github@diaspoir.net>
2024-04-22 15:57:02 +0800

    Cleaned up the automatic scrubber.

--- 98-btrfs-balance-scrub.sh
+++ 98-btrfs-balance-scrub.sh
@@ -12,16 +12,16 @@ if [ "$pools" != "" ] ; then
     echo Found these pools: $pools
 
     for pool in $pools; do
-	echo "Starting $btrfs scrub start $pool"
-	# turn off the quotas before balancing
-	$btrfs quota disable $pool
-	$btrfs filesystem sync $pool
-	# Balance Data and Metadata - empty blocks only possibly unnecessary
-	$btrfs balance start -musage=0 -dusage=0 -v $pool
-	# Balance Data limit the number of of Blocks. Metadata is automatic now.
-	$btrfs balance start -dlimit=20 -v $pool
-	# Start the Scrub
-	$btrfs scrub start $pool
+        echo "Starting $btrfs scrub start $pool"
+        # turn off the quotas before balancing
+        $btrfs quota disable $pool
+        $btrfs filesystem sync $pool
+        # Balance Data and Metadata - empty blocks only possibly unnecessary
+        $btrfs balance start -musage=0 -dusage=0 -v $pool
+        # Balance Data limit the number of of Blocks. Metadata is automatic now.
+        $btrfs balance start -dlimit=20 -v $pool
+        # Start the Scrub
+        $btrfs scrub start $pool
     done
 
     echo "BTRFS automatic scrub start done.  Use '$btrfs scrub status $pool' to see progress."
@@ -29,6 +29,3 @@ fi
 
 # Wait for the pools to finish and reenable the quotas?
 FINISHED=0
-
-# Sleep for 
-#/home/odaiwai/src/backup_and_file_scripts/check_scrub_status.pl
